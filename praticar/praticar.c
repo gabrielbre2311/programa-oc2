a96e83ee2ead0e08fe09a02abbb00e958a1a57fa
gabrielbre2311 <gbreves4@gmail.com>
2025-05-08 01:00:36 +0000

    super trunfo desafio

---
+++ praticar/praticar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+   int idade, matricula;
+   float altura;
+   char nome[50];
+
+
+
+   printf("digite sua idade: \n");
+   scanf("%d", &idade);
+
+   printf("digite sua altura: \n");
+   scanf("&f", &altura); 
+   
+   printf("digite o seu nome: \n");
+   scanf("%s", &nome);
+
+   printf("digite sua matricula: \n");
+   scanf("%d", &matricula);
+
+   printf("nome do aluno: %s - matricula %d \n", nome , matricula);
+   printf("idade: %d - altura: %f \n", idade, altura);
+
+
+   return 0;
+
+}
